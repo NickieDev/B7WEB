43ef9ce727d317faf3d49764cbc1b814bfad365a
Nick <nickiedev@outlook.com>
2024-07-31 16:30:28 -0300

    Upload das aulas 19 à 21 do Modulo 01 de React Native

---
+++ (13) React Native/(19) Projeto - Loja Basica - Tela Principal/loja_basica/App.tsx
@@ -0,0 +1,46 @@
+import { View, SafeAreaView, StatusBar, StyleSheet, Image, Text, FlatList } from 'react-native'
+import { list } from './data'
+
+function App() {
+  return(
+    <SafeAreaView style={ styles.container }>
+      <Image
+        source={ require('./assets/hero.jpg') }
+        resizeMode='cover'
+        style={ styles.hero }
+      />
+
+      <View style={ styles.area }>
+        <Text style={ styles.h1 }>Produtos</Text>
+
+        <FlatList 
+          data={ list }
+          renderItem={  }
+          keyExtractor={ item => item.id.toString() }
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight || 0
+  },
+
+  hero: {
+    width: '100%',
+    height: 120
+  },
+
+  area: {
+    padding: 10
+  },
+
+  h1: {
+    fontSize: 24,
+    marginBottom: 10
+  }
+})
+
+export default App
---
+++ (13) React Native/(20) Loja Basica - Dados e Types/loja_basica/components/product-item.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native'
+
+export const ProductItem = () => {
+  return(
+    <View>
+    </View>
+  )
+}
